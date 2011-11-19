cf0f0a1b77811005859cf5dfc41c0626d4831387
Haotian Liu <haotianliu@Haotians-MacBook-Pro.local>
2011-11-19 00:18:58 -0600

    Add the data structure and bitmap related functions

--- sfs.c
+++ sfs.c
@@ -23,6 +23,42 @@
 #include "sdisk.h"
 
 /*
+ *	global variables
+ */
+
+#define MAXINODE	2000//	minimun should be SD_NUMSECTORS/7, but the wores case is: each file takes one iNode, so the total number will reach 2000
+#define MAXFPTAB	2000//	for file descriptor table, it is in memory
+
+typedef struct inode_t {//	i-node structure
+	//	some attributes
+	int		dir;//	bool, 1 means it is a directory
+	void*	toblock[7];
+	void*	toinode;// to next inode
+} inode;
+
+typedef struct fptab_t {// file descriptor sturcture in memory
+	void*	fptab[MAXFPTAB];
+} fptab;
+
+typedef struct file_t {// file sturcture for file header, it is a file sturcture in the sector
+	char	name[17];// support for up to 16 characteristics, the last one should be \0
+	inode*	inode;// point back to its inode. whether it is a ture file ot a directory is defined in inode.
+} file;
+
+typedef struct disk_t {// disk sturcture for disk header
+	
+	inode			inode[MAXINODE];
+	unsigned char	bitmap[SD_NUMSECTORS/8];
+	inode			root;// reserve for root
+} disk;
+
+disk*	maindisk;
+fptab*	mainfptab;
+
+void fillbitmap(int sector);
+void emptybitmap(int sector);
+
+/*
  * sfs_mkfs: use to build your filesystem
  *
  * Parameters: -
@@ -161,3 +197,13 @@ int sfs_rm(char *file_name) {
     // TODO: Implement for extra credit
     return -1;
 } /* !sfs_rm */
+
+void fillbitmap(int sector){
+	unsigned char* bitmap=(*maindisk).bitmap;
+	bitmap[sector/8] |= (1<<(sector%8));
+}
+
+void emptybitmap(int sector){
+	unsigned char* bitmap=(*maindisk).bitmap;
+	bitmap[sector/8] &= (~(1<<(sector%8)));
+}
