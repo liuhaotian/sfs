9cc626f481a5e7465bd434ab4ebfa955b9d1f18b
Haotian Liu <haotianliu@Haotians-MacBook-Pro.local>
2011-11-26 17:29:45 -0600

    Fix the returning again

--- sfs.c
+++ sfs.c
@@ -542,7 +542,7 @@ int sfs_fwrite(int fileID, char *buffer, int length) {
 			(*maindisk).inode[inode].size = (*mainfptab).pos[i];
 			
 			free(thisfile);
-			return ( (*maindisk).inode[inode].size - prevsize);
+			return (((*maindisk).inode[inode].size - prevsize) > length)?((*maindisk).inode[inode].size - prevsize):length;
 		}
 		return -1;
 } /* !sfs_fwrite */
